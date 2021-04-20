019dfac7a89b3a5fd76b043816ad092cd1c4f649
Jakeman582 <jakehiance@gmail.com>
2021-04-20 11:19:23 -0400

    Add files via upload
    Added jobs that can see what words have been used in titles globally and per channel

---
+++ Java/TitleWordCounterReducer.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class TitleWordCounterReducer extends Reducer<Text, IntWritable, Text, NullWritable>{
+	
+	// We want a length cutoff
+	private final int LENGTH_CUTOFF = 2;
+	
+	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException{
+		if(key.toString().length() > LENGTH_CUTOFF) {
+			context.write(key, NullWritable.get());
+		}
+	}
+
+}
