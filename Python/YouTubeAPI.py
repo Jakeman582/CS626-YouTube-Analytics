8cf5ee65474744a12cefdc8cce736dfe6e1cd0b2
Jakeman582 <jakehiance@gmail.com>
2021-05-04 21:54:58 -0400

    Add files via upload

--- Python/YouTubeAPI.py
+++ Python/YouTubeAPI.py
@@ -1,7 +1,8 @@
 import math
 import json
-from urllib.error import HTTPError
+import sys
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 def call(file_name = "MrBeast", user_name = "MrBeast6000"):
 
@@ -9,11 +10,16 @@ def call(file_name = "MrBeast", user_name = "MrBeast6000"):
 	service = build("youtube", "v3", developerKey = "AIzaSyCtL1EJnBSOpsSfr0hPZAzbEJl9SXuMtmI")
 
 	# We need to store the data retrieved from the API
-	file_path = "C:\\data\youtube_api_data\\" + file_name + ".txt"
+	file_path = "C:\\\\data\\youtube_api_data\\" + file_name + ".txt"
 	file_mode = "w"
 	file_encoding = "utf-8"
 
+	#########################################################################
 	### Querying the YouTube API to get a Channel's 'uploads' playlist ID ###
+	#########################################################################
+
+	# LOGGING: Print the username whose videos we are getting
+	print("Fetching \"uploads\" playlist ID for " + file_name)
 
 	# The maximum number of elements per page of response
 	# is capped at 50 per the YouTube API documentation
@@ -22,16 +28,31 @@ def call(file_name = "MrBeast", user_name = "MrBeast6000"):
 	# Setting what content to retrieve from the API
 	channel_name = user_name
 	channel_part = "contentDetails,statistics"
-	channel_request = service.channels().list(part = channel_part, forUsername = channel_name)
+
+	# If the first two characters are "UC", then it's a channel ID, not a username
+	if(user_name.startswith("UC")):
+		channel_request = service.channels().list(part = channel_part, id = channel_name)
+	else:
+		channel_request = service.channels().list(part = channel_part, forUsername = channel_name)
 
 	# Execute the request
 	try:
 		channel_response = channel_request.execute()
-	except HTTPError as e:
+
+		# LOGGING: Fetched the 'uploads' playlist
+		print("Fetched \"uploads\" playlist ID for " + file_name)
+
+	except HttpError as e:
 		print("Error response status code: {0}, reason: {1}".format(e.resp.status, e.error_details))
+		sys.exit(0)
 
 
+	################################################################
 	### Querying the API to get the channel's uploaded video IDs ###
+	################################################################
+
+	# LOGGING: Querying the API to get the uploaded video IDs
+	print("Fetching video IDs for " + file_name)
 
 	# Setting what content to retrieve from the API
 	playlist_id = channel_response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
@@ -39,7 +60,7 @@ def call(file_name = "MrBeast", user_name = "MrBeast6000"):
 	playlist_page_token = ""
 
 	# We need to store each video ID so we can search it later
-	video_ids = []
+	video_id_set = set()
 
 	# We need to know how many times to make the request
 	video_count = int(channel_response['items'][0]['statistics']['videoCount'])
@@ -47,8 +68,10 @@ def call(file_name = "MrBeast", user_name = "MrBeast6000"):
 	
 	# Execute the new request
 	try:
-		# Process each page of the response
+
+		# process each page of the response
 		for i in range(page_count):
+
 			# Construct and execute the request
 			playlist_request = service.playlistItems().list(
 				part = playlist_part, 
@@ -59,25 +82,52 @@ def call(file_name = "MrBeast", user_name = "MrBeast6000"):
 			playlist_response = playlist_request.execute()
 
 			# Update the page token if not on the last page
-			if(i != (page_count - 1)):
-				playlist_page_token = playlist_response['nextPageToken']
+			if('nextPageToken' in playlist_response):
+				if(playlist_page_token != playlist_response['nextPageToken']):
+					playlist_page_token = playlist_response['nextPageToken']
 
-			# Add a new element to the string array
-			video_ids.append("")
-
-			# Add each video ID to a string array (needed when searching for videos)
+			# Add each video id to a string array (needed when searching for videos)
 			for video_element in playlist_response['items']:
-				video_ids[i] += video_element['snippet']['resourceId']['videoId']
-				video_ids[i] += ","
+				current_video = video_element['snippet']['resourceId']['videoId']
 
-			# Remove the trailing comma
-			video_ids[i] = video_ids[i][:-1]
+				if(current_video not in video_id_set):
+					video_id_set.add(current_video)
 
-	except HTTPError as e:
+		# DEBUG: CHECK TO SEE IF CNN PRODUCED 151282 VIDEO IDS
+		print("Fetched video IDs for " + file_name)
+
+	except HttpError as e:
 		print("Error response status code: {0}, reason: {1}".format(e.resp.status, e.error_details))
 
+	except KeyError as e:
+		print("" + playlist_response)
 
-	### Querying the API to get the channel's uploaded video IDs ###
+	#####################################################################
+	### Querying the API to get the channel's uploaded video metadata ###
+	#####################################################################
+
+	# Logging: Getting metadata for each video
+	print("Fetching video metadata for " + file_name)
+
+	# Split the set of video ids into groups of 50 to reduce quota strains
+	video_ids = []
+	count = 0
+	id_string = ""
+	for id in video_id_set:
+		id_string += id
+		id_string += ","
+		count += 1
+		if(count >= results_per_page):
+			id_string = id_string[:-1]
+			video_ids.append(id_string)
+			id_string = ""
+			count = 0
+
+	# Take care of any partially full pages not handled by the above loop
+	if(count != 0):
+		id_string = id_string[:-1]
+		video_ids.append(id_string)
+
 
 	# Setting what content to get from the API
 	video_part = "snippet,statistics"
@@ -134,7 +184,10 @@ def call(file_name = "MrBeast", user_name = "MrBeast6000"):
 						file = output_file
 					)
 
-		except HTTPError as e:
+			# LOGGING: FEtched video metadata
+			print("Fetched video metadata for " + file_name)
+
+		except HttpError as e:
 			print("Error response status code: {0}, reason: {1}".format(e.resp.status, e.error_details))
 
 	finally:
--- Python/scrape_data.py
+++ Python/scrape_data.py
@@ -1,19 +1,46 @@
 import sys
+from pathlib import Path
+
 import YouTubeAPI
+
+def check_present_files(data_directory):
+
+	path = Path(data_directory)
+
+	# Get each YouTuber's file from the directory
+	return [file.stem for file in path.iterdir() if file.is_file()]
+
 
 def run(input_file = ""):
 	
-	# If no file is supplied, call the default call method for the YouTube API
+	# If no file is suplied, call the default call method for the YouTube API
 	if(input_file == ""):
 		YouTubeAPI.call()
+
+	# Here, we need to get the username and filename from the input file
 	else:
-		# Here, we need to get the username and filename from the input file
+
+		# Make sure we only put in files for YouTubers we have not 
+		# retrieved data for, which should help ease quota usage
+		files = check_present_files("C:\\\\data\\youtube_api_data\\")
+		print(files)
+		
 		with open(file = input_file, mode = "r", encoding = "utf-8") as input:
 			input_lines = input.readlines()
 			for line in input_lines:
-				line.strip()
-				filename, username = line.split(",")
-				YouTubeAPI.call(filename, username)
+
+				# We can ignore lines that start with #, and any empty lines
+				#if(not line.startswith("#") and length(line) != 0):
+				if(not line.startswith("#")):
+
+					# Get the common name and user name for each YouTuber
+					line.strip()
+					file_name, user_name = line.split(",")
+
+					# Make sure we haven't yet processed this YouTuber's
+					# channel
+					if(file_name not in files):
+						YouTubeAPI.call(file_name, user_name)
 
 if __name__ == "__main__":
 	if(len(sys.argv) <= 1):
